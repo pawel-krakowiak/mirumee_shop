6fa4d2262568197a541209cb14bc6051b9ea059f
FuRaJ3003 <FurajChirashi3003@gmail.com>
2021-05-25 19:27:21 +0200

    Created checkout model

--- shop/checkout/models.py
+++ shop/checkout/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.conf import settings
+from datetime import datetime as dt
 
-# Create your models here.
+class Checkout(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user_email = model.EmailField(max_lenght=100, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user} - {dt.now().strftime("%Y/%m/%d %H:%M:%S")}"
