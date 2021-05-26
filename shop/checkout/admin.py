91fb9b91e9d8cd8f64eb44baf4f8d9802135241f
FuRaJ3003 <FurajChirashi3003@gmail.com>
2021-05-26 13:44:22 +0200

    Fixed import typo

--- shop/checkout/admin.py
+++ shop/checkout/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Checkout, CheckoutLines
+from .models import Checkout, CheckoutLine
 
 admin.site.register(Checkout)
-admin.site.register(CheckoutLines)
+admin.site.register(CheckoutLine)
