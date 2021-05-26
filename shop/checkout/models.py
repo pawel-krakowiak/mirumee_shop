e2914689ef22cc32ad49b18c38edbcf6c0be07e2
FuRaJ3003 <FurajChirashi3003@gmail.com>
2021-05-26 15:52:14 +0200

    Switched realtions (1:n > n:1)

--- shop/checkout/models.py
+++ shop/checkout/models.py
@@ -4,19 +4,19 @@ from datetime import datetime as dt
 
 from ..product.models import ProductVariant
 
-class Checkout(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    user_email = models.EmailField(max_length=100, null=True, blank=True)
-    # lines = models.ForeignKey(CheckoutLine, related_name='lines', on_delete=models.CASCADE)
-    
-    def __str__(self):
-        return f"User Checkout: {self.user}"
-
 class CheckoutLine(models.Model):
-    checkout = models.ForeignKey(Checkout, related_name='checkout', on_delete=models.CASCADE)
+    # checkout = models.ForeignKey(Checkout, related_name='checkout', on_delete=models.CASCADE)
     variant = models.ForeignKey(ProductVariant, related_name='+', on_delete=models.CASCADE)
-    # variant_price = models.ForeignKey(ProductVariant, related_name='prices', on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
 
     def __str__(self):
-        return f"{self.checkout} > Variant: {self.variant} Quantity: {self.quantity}"
+        return f"> Variant: {self.variant} Quantity: {self.quantity}"
+
+class Checkout(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user_email = models.EmailField(max_length=100, null=True, blank=True)
+    lines = models.ForeignKey(CheckoutLine, related_name='lines', on_delete=models.CASCADE)
+    # lines = models.ForeignKey(CheckoutLine, related_name='checkout', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"User Checkout: {self.user}"
