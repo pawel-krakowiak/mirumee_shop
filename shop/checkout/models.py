2918497a5a1f8e9623afc230559d7a38454b3999
FuRaJ3003 <FurajChirashi3003@gmail.com>
2021-05-26 13:45:21 +0200

    Fixed FK's bug

--- shop/checkout/models.py
+++ shop/checkout/models.py
@@ -2,13 +2,15 @@ from django.db import models
 from django.conf import settings
 from datetime import datetime as dt
 
+from ..product.models import ProductVariant
+
 class Checkout(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    user_email = model.EmailField(max_lenght=100, null=True, blank=True)
-    lines = models.ForeignKey(CheckoutLine, related_name='lines', on_delete=models.CASCADE)
+    user_email = models.EmailField(max_length=100, null=True, blank=True)
+    # lines = models.ForeignKey(CheckoutLine, related_name='lines', on_delete=models.CASCADE)
     
     def __str__(self):
-        return f"{self.user} - {dt.now().strftime("%Y/%m/%d %H:%M:%S")}"
+        return f"{self.user} - {dt.now.strftime('%m/%d/%Y, %H:%M:%S')}"
 
 class CheckoutLine(models.Model):
     checkout = models.ForeignKey(Checkout, related_name='checkout', on_delete=models.CASCADE)
