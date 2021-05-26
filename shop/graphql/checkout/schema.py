bbeb2b81f4f74d720b146f7c0f4093bcd2131b25
FuRaJ3003 <FurajChirashi3003@gmail.com>
2021-05-26 13:30:39 +0200

    Defined checkout and checkout lines queries

---
+++ shop/graphql/checkout/schema.py
@@ -0,0 +1,34 @@
+import graphene
+
+from .types import CheckoutType, CheckoutLineType
+from ...checkout.models import Checkout, CheckoutLine
+from .mutation import CheckoutCreate, CheckoutLineCreate
+
+class CheckoutQueries(graphene.ObjectType):
+    checkout = graphene.Field(
+        CheckoutType, id=graphene.Argument(graphene.ID, description="ID of checkout.")
+    )
+    checkouts = graphene.List(CheckoutType)
+    checkout_line = graphene.FIeld(
+        CheckoutLineType,
+        id=graphene.Argument(graphene.ID, description="ID of checkout line.")
+    )
+
+    def resolve_checkout(self, _info, id):
+        return Checkout.objects.filter(id=id).first()
+
+    def resolve_checkouts(self, _info):
+        return Checkout.objects.all()
+
+    def resolve_checkout_line(self, _info, id):
+        return CheckoutLine.objects.filter(id=id).first()
+
+    def resolve_checkout_lines(self, _info, id):
+        return CheckoutLine.objects.all()
+
+
+class CheckoutMutation(graphene.ObjectType):
+    checkout_create = CheckoutCreate.Field()
+    checkout_variant_create = CheckoutLineCreate.Field()
+
+
