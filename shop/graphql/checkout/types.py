dbc6c95e520d92fb69e32d4c0ef196c1ef7a79d3
FuRaJ3003 <FurajChirashi3003@gmail.com>
2021-05-26 13:03:28 +0200

    Created checkout and checkout lines graphql types

--- shop/graphql/checkout/types.py
+++ shop/graphql/checkout/types.py
@@ -1,11 +1,11 @@
 import graphene
-from graphene_django import DjangoObjectType
 from django.db.models import Sum
+from grapehene_django import DjangoObjectType
 
 from ...checkout.models import Checkout, CheckoutLine
 
 class CheckoutType(DjangoObjectType):
-    total_price = graphene.Decimal(description="Total price of products in checkout.")
+    total_price = graphene.Decimal(description='Total price of checkout.')
 
     class Meta:
         model = Checkout
@@ -13,13 +13,14 @@ class CheckoutType(DjangoObjectType):
 
     def resolve_total_price(self, _info):
         checkout = self.variants.all().aggregate(total_variant_price=Sum('price'))
-        total_variant_price = checkout['total_variant_price']
-        if not total_variant_price:
+        total_variant_price = product['total_variant_price']
+        if not total_variant_Price:
             return self.price
 
-        return total_variant_price
+        return product['total_variant_price']
 
 class CheckoutLineType(DjangoObjectType):
     class Meta:
         model = CheckoutLine
         fields = '__all__'
+        
