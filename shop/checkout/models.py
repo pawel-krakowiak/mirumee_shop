8cd183040639a921c849b19b39d6101480abcefa
FuRaJ3003 <FurajChirashi3003@gmail.com>
2021-05-26 14:17:19 +0200

    Removed FK from checkout model

--- shop/checkout/models.py
+++ shop/checkout/models.py
@@ -10,12 +10,12 @@ class Checkout(models.Model):
     # lines = models.ForeignKey(CheckoutLine, related_name='lines', on_delete=models.CASCADE)
     
     def __str__(self):
-        return f"{self.user} - {dt.now.strftime('%m/%d/%Y, %H:%M:%S')}"
+        return f"User Checkout: {self.user}"
 
 class CheckoutLine(models.Model):
     checkout = models.ForeignKey(Checkout, related_name='checkout', on_delete=models.CASCADE)
-    variant = models.ForeignKey(ProductVariant, related_name='name', on_delete=models.CASCADE)
-    variant_price = models.ForeignKey(ProductVariant, related_name='price', on_delete=models.CASCADE)
+    variant = models.ForeignKey(ProductVariant, related_name='+', on_delete=models.CASCADE)
+    # variant_price = models.ForeignKey(ProductVariant, related_name='prices', on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
 
     def __str__(self):
