09091fa53547fad2ae5d6221bcf3946833dfc47e
FuRaJ3003 <FurajChirashi3003@gmail.com>
2021-05-27 18:32:27 +0200

    Values str display depends on attributes

--- shop/checkout/models.py
+++ shop/checkout/models.py
@@ -5,16 +5,21 @@ from datetime import datetime as dt
 from ..product.models import ProductVariant
 
 class Checkout(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True)
     user_email = models.EmailField(max_length=100, null=True, blank=True)
-    
+
     def __str__(self):
-        return f"User Checkout: {self.user}"
+        if self.user_email:
+            if self.user != None:
+                return f"Checkout: {self.user_email} ID: {self.id} - User: ({self.user})"
+            return f"Checkout: {self.user_email} ID: {self.id}"
+        else:
+            return f"Checkout: (Uknown Owner) ID: {self.id}"
 
 class CheckoutLine(models.Model):
-    checkout = models.ForeignKey(Checkout, related_name='lines', on_delete=models.CASCADE)
+    checkout = models.ForeignKey(Checkout, related_name='lines', on_delete=models.CASCADE, null=True)
     variant = models.ForeignKey(ProductVariant, related_name='+', on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
 
     def __str__(self):
-        return f"> Variant: {self.variant} Quantity: {self.quantity}"
+        return f"ID: {self.id} Variant: {self.variant} Quantity: {self.quantity}"
