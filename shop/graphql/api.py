6e0dbe33bf4ddecd9a95c8b44d6910a2e106b686
FuRaJ3003 <FurajChirashi3003@gmail.com>
2021-06-09 07:40:49 +0200

    Implemented user queries and mutations into api

--- shop/graphql/api.py
+++ shop/graphql/api.py
@@ -3,6 +3,7 @@ import graphene
 from .product.schema import ProductQueries, ProductMutations
 from .checkout.schema import CheckoutMutations, CheckoutQueries
 from .account.authenticate import AuthenticateMutations
+from .account.schema import UserQueries, UserMutations
 
 
 class Query(ProductQueries, CheckoutQueries):
