14040921ca10ab0419cfb40ff9d625483c908245
FuRaJ3003 <FurajChirashi3003@gmail.com>
2021-06-09 11:44:13 +0200

    Registered user in admin panel

--- shop/settings.py
+++ shop/settings.py
@@ -141,7 +141,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 GRAPHENE = {
-    "SCHEMA": "shop.graphql.api.schema",
+    'SCHEMA': "shop.graphql.api.schema",
     'MIDDLEWARE': [
         'graphql_jwt.middleware.JSONWebTokenMiddleware',
     ],
