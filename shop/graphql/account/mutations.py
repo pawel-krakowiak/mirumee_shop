789b07e870426e0b2708790bfb732702b7c590b6
FuRaJ3003 <FurajChirashi3003@gmail.com>
2021-06-09 08:13:34 +0200

    Improved user exists error handling

--- shop/graphql/account/mutations.py
+++ shop/graphql/account/mutations.py
@@ -1,5 +1,7 @@
 import graphene
 from graphql import GraphQLError
+from django.core.exceptions import ObjectDoesNotExist
+
 from ...account.models import User, UserManager
 from .types import UserType
 
@@ -8,7 +10,7 @@ class UserCreateInput(graphene.InputObjectType):
     email = graphene.String(required=True, description="User e-mail (username field)")
     first_name = graphene.String(required=False, description="First name of user")
     last_name = graphene.String(required=False, description="Last name of user")
-    is_active = graphene.BooleanField(required=False, default=True, description="Optionally create an unactive user")
+    is_active = graphene.Boolean(required=False, default=True, description="Optionally create an unactive user")
 
 
 class UserCreate(graphene.Mutation):
@@ -21,20 +23,12 @@ class UserCreate(graphene.Mutation):
         # TODO: Validate email
         return input
 
-    def check_user_exist(cls, email):
-        try:
-            Users.objects.get(email=email)
-        except ObjectDoesNotExists():
-            return False
-        else:
-            return True
             
     @classmethod
     def mutate(cls, root, info, input):
         cleaned_input = cls.clean_input(input)
-        is_exist = cls.check_user_exist(input.get("email"))
 
-        if not is_exist:
+        if not User.objects.get(email=input.get('email')):
             user = User.objects.create(
                 is_staff=False,
                 is_superuser=False,
@@ -44,11 +38,11 @@ class UserCreate(graphene.Mutation):
             return UserCreate(user=user)
 
         else:
-            raise GraphQLError('That email already exists!')
-            return
+            raise GraphQLError(f'{cls.__name__} Error: That email already exists!')
+            return None
         
         
-class StaffUserCreate(graphene.Mutation, UserCreate):
+class StaffUserCreate(graphene.Mutation):
     user = graphene.Field(UserType)
 
     class Arguments:
@@ -57,9 +51,8 @@ class StaffUserCreate(graphene.Mutation, UserCreate):
     @classmethod
     def mutate(cls, root, info, input):
         cleaned_input = UserCreate.clean_input(input)
-        is_exist = UserCreate.check_user_exist(input.get("email"))
 
-        if not is_exist:
+        if not User.objects.get(email=input.get('email')):
             user = User.objects.create(
                 is_staff=True,
                 is_superuser=False,
@@ -69,6 +62,6 @@ class StaffUserCreate(graphene.Mutation, UserCreate):
             return StaffUserCreate(user=user)
 
         else:
-            raise GraphQLError('That email already exists!')
-            return
+            raise GraphQLError(f'{cls.__name__} Error: That email already exists!')
+            return None
 
