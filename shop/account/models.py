c8b321349be39167a6255b8a12a38afcd7844ca2
FuRaJ3003 <FurajChirashi3003@gmail.com>
2021-06-09 06:39:26 +0200

    Created account model

--- shop/account/models.py
+++ shop/account/models.py
@@ -3,29 +3,32 @@ from django.contrib.auth.models import (
     BaseUserManager,
     PermissionsMixin,
 )
-
 from django.db import models
 
+
 class UserManager(BaseUserManager):
-    def create_user(self, email, first_name, last_name, password=None):
-        if not email:
-            raise ValueError('Ustaw e-mail dzbanie')
-        user = self.model(email=email, first_name=first_name, last_name=last_name)
-        user.set_password(password)
-        user.is_staff = False
-        user.save(using=self._db)
+    def create_user(self,email, password=None, is_active=True, **extra_fields):
+        email = UserManager.normalize_email(email)
+
+        extra_fields.pop('username',None)
+
+        user = self.model(
+            email=email,
+            is_active=is_active,
+            **extra_fields
+        )
+
+        if password:
+            user.set_password(password)
+
+        user.save()
         return user
 
-    def create_superuser(self, email, first_name, last_name, password):
-        user = self.create_user(email, first_name, last_name, password)
-        user.is_admin = True
-        user.save(using=self._db)
-        return user
+    def create_superuser(self, email, password=None, **extra_fields):
+        return self.create_user(email, password,is_staff=True, is_superuser=True, **extra_fields)
 
-    def get_by_natural_key(self, email_):
-        return self.get(code_number=email_)
 
-class User(AbstractBaseUser, PermissionsMixin):
+class User(PermissionsMixin, AbstractBaseUser):
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=256, blank=True)
     last_name = models.CharField(max_length=256, blank=True)
@@ -33,15 +36,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
 
-    objects = UserManager
-
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
 
-    def natural_key(self):
-        return self.email
-    
-    class Meta:
-        db_table = 'user'
-
-
+    objects = UserManager()
